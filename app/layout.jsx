1f749a4a1777d3fb9fa993c02a26417f1497ba6f
anurag-10-git <anuragmohan2022@gmail.com>
2025-09-18 00:30:14 +0530

    intial commit

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import '@/assets/styles/globals.css'
+
+export const metadata = {
+  title: 'Property Pulse',
+  keywords: 'rental, property, real estate',
+  description: 'find the perfect rental property'
+}
+
+const MainLayout = ({ children }) => {
+  return (
+    <html>
+      <body>
+        <main>
+          {children}
+        </main>
+      </body>
+    </html>
+  )
+}
+
+export default MainLayout
