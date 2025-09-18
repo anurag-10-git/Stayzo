754472db05805c81dcf9b8af7fa21b0d9327488d
anurag-10-git <anuragmohan2022@gmail.com>
2025-09-18 23:20:38 +0530

    adding routes

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,12 @@
+import Link from 'next/link'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div className='text-2xl'>HomePage</div>
+    <div>
+      <h1 className='text-3xl'>HomePage</h1>
+      <Link href='/properties'>Go to Properties</Link>
+    </div>
   )
 }
 
